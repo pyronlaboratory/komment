0c7d685dc1762c3c34d558d1e8f9dc75d7e7c665
Ronnie <ronnie94official@gmail.com>
2023-12-05 11:46:12 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+function f0() { return 1; };
+
+f1(number) {
+  return number * number;
+};
+  
+f2 = function (number) {
+  return number * number;
+};
+
 f3 = (number) => {
   return number * number;
 };
