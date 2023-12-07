6e6808ccec8eb8dbff4b68ea0951e53b514046eb
Ronnie <ronnie94official@gmail.com>
2023-12-07 10:44:58 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
-/**
-* @description Nothing. The function simply defines a variable `f0` and doesn't
-* perform any actual operations or return anything.
-*/
-function f0() { console.log("dummy"); };
-
-// f1(number) {
-//   return number * number;
-// };
-  
-// f2 = function (number) {
-//   return number * number;
-// };
-
-// f3 = (number) => {
-//   return number * number;
-// };
+f0 = () => { console.log("dummy"); };
